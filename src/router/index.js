c29af7a4799849742e8df9358a2e50542258dee9
xiaoxiaoweii <2679656468@qq.com>
2020-04-11 18:38:44 +0800

    路由守卫判断是否登录

--- src/router/index.js
+++ src/router/index.js
@@ -27,4 +27,19 @@ const router = new VueRouter({
   routes
 })
 
+
+// 挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转
+  // next 是一个函数 表示放行 next() 放行 next('/login') 强制跳转指定路由
+  // 如果访问的是登录页 则不需要做权限控制 直接放行
+  if(to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果token不存在 返回登录页
+  if(!tokenStr) return next('/login')
+  // 如果登录存在 则放行
+  next()
+})
 export default router
