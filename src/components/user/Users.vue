68ef55d04462cd60f4b92fc378b12d422fd55f48
xiaoxiaoweii <2679656468@qq.com>
2020-04-12 01:00:19 +0800

    用户管理表格 分页 搜索

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,13 +1,152 @@
 <template>
   <div>
-    <h3>用户</h3>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card class="box-card">
+      <!-- 搜索与添加区域 -->
+      <div style="margin-top: 15px;">
+        <el-row :gutter="20">
+          <el-col :span="10">
+            <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+              <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+            </el-input>
+          </el-col>
+          <el-col :span="4">
+            <el-button type="primary">添加用户</el-button>
+          </el-col>
+        </el-row>
+      </div>
+      <!-- 用户列表区域 -->
+      <el-table :data="userList" border stripe>
+        <el-table-column label="#" type="index" width="50px" align="center"></el-table-column>
+        <el-table-column label="姓名" prop="username" align="center"></el-table-column>
+        <el-table-column label="邮箱" prop="email" align="center"></el-table-column>
+        <el-table-column label="电话" prop="mobile" align="center"></el-table-column>
+        <el-table-column label="角色" prop="role_name" align="center"></el-table-column>
+        <el-table-column label="状态" align="center">
+          <template v-slot="scope">
+            <el-switch
+              v-model="scope.row.mg_state"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              @change="userStateChanged(scope.row)"
+            ></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="300px" align="center">
+          <template>
+            <!-- 修改按钮 -->
+            <el-tooltip effect="dark" content="编辑" placement="top-start" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            </el-tooltip>
+            <!-- 删除按钮 -->
+            <el-tooltip effect="dark" content="删除" placement="top-start" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </el-tooltip>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top-start" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <div class="block">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-sizes="[1, 2, 5, 10]"
+          :page-size="queryInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+    </el-card>
   </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        // 字符串 信息
+        query: '',
+        // 当前页码
+        pagenum: 1,
+        // 当前每页显示数据条数
+        pagesize: 5
+      },
+      // 用户列表
+      userList: [],
+      // 用户总条数
+      total: 0
+
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    // 获取用户列表
+    async getUserList () {
+      const { data: res } =
+        await this.$http.get('users', {
+          params: this.queryInfo
+        })
+      // 如果获取用户信息失败 即返回status不是200
+      if (res.meta.status !== 200) return this.$Notification({
+        title: 'error',
+        message: '获取用户列表失败!',
+        type: 'error',
+        duration: 1000
+      })
+      this.userList = res.data.users
+      this.total = res.data.total
+      // console.log(this.userList)
+    },
+    // 监听 page-sizes 改变的事件
+    handleSizeChange (newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听页码值改变的事件
+    handleCurrentChange (newPage) {
+      // console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+    // 用户状态变化回调函数
+    async userStateChanged (userInfo) {
+      // console.log(userInfo)
+      const { data: res } = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
+      if (res.meta.status !== 200) {
+        // 处理失败 将状态取反
+        userInfo.mg_state = !userInfo.mg_state
+        return this.$Notification({
+          title: 'error',
+          message: '更新用户状态失败！',
+          type: 'error',
+          duration: 1000
+        })
+      }
+      this.$Notification({
+          title: 'success',
+          message: '更新用户状态成功！',
+          type: 'success',
+          duration: 1000
+        })
+    }
+  }
+
 }
 </script>
 <style lang="less" scoped>
-
 </style>
